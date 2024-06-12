72662f2345f74761fb556fd822c19bdba4550ec0
ethramos22 <ethramos22@gmail.com>
2024-06-12 16:45:44 -0600

    unneeded imports

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import placeholderIMG from '../assets/legoPlaceholder.jpg'
 // Bootstrap
-import { Card, Col, Button, ButtonGroup } from 'react-bootstrap';
+import { Card, Col, Button} from 'react-bootstrap';
 
 export default function ProjectCard({project}) {
     return (
