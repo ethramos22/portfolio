41c30c0b1d308a530dfbd998eadccd4d5d821921
ethramos22 <ethramos22@gmail.com>
2024-06-12 18:09:30 -0600

    Updates

---
+++ precache-manifest.183441205a073dfbb6e093c24812d8a4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "256346c54d31afb9709c",
+    "url": "/portfolio/static/css/main.a4273a30.chunk.css"
+  },
+  {
+    "revision": "256346c54d31afb9709c",
+    "url": "/portfolio/static/js/main.256346c5.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/portfolio/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "852658af695c906ad9e2",
+    "url": "/portfolio/static/css/2.3eb8c954.chunk.css"
+  },
+  {
+    "revision": "852658af695c906ad9e2",
+    "url": "/portfolio/static/js/2.852658af.chunk.js"
+  },
+  {
+    "revision": "b8e6e81ad7286bca6ca5",
+    "url": "/portfolio/static/js/runtime~main.b8e6e81a.js"
+  },
+  {
+    "revision": "1868aacb699b54556b35d181cf81951e",
+    "url": "/portfolio/static/media/legoPlaceholder.1868aacb.jpg"
+  },
+  {
+    "revision": "8d4f12ddc2d9edee524c6e4529ad8ef4",
+    "url": "/portfolio/static/media/FireBallToolsLogo.8d4f12dd.png"
+  },
+  {
+    "revision": "7fc5862b31b02bf9f59a3e031d683fd9",
+    "url": "/portfolio/static/media/Players Tab.7fc5862b.png"
+  },
+  {
+    "revision": "53edbd406c8cf46b59a76d88e3568349",
+    "url": "/portfolio/static/media/Characters home.53edbd40.png"
+  },
+  {
+    "revision": "8bd8e5ba9401306e2a36dabef236c40f",
+    "url": "/portfolio/static/media/Sample Encounter.8bd8e5ba.png"
+  },
+  {
+    "revision": "11221583659db5eab6d1b10cf6b64b1d",
+    "url": "/portfolio/static/media/Encounter Builder.11221583.png"
+  },
+  {
+    "revision": "650aaa0603fe904e8ef6ea7c6b15cbc0",
+    "url": "/portfolio/static/media/Search Monster.650aaa06.png"
+  },
+  {
+    "revision": "421d5e2ee82d06ec75a1dfb2fa2cf61a",
+    "url": "/portfolio/static/media/Spell Encounter Home.421d5e2e.png"
+  },
+  {
+    "revision": "ee84ec5080e2e23d3a7386a8bf8e7ae3",
+    "url": "/portfolio/index.html"
+  }
+];
