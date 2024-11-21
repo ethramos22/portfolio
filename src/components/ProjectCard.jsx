26897a2320c2a676070ad6c5dd705fa1014360ec
ethramos22 <ethramos22@gmail.com>
2024-11-20 21:44:03 -0700

    modal with image slider

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import { useState } from 'react';
 // Bootstrap
-import { Card, Col, Button} from 'react-bootstrap';
+import { Card, Col, Button, Modal, Carousel} from 'react-bootstrap';
 
-export default function ProjectCard({project}) {
+export default function ProjectCard({ project }) {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
-        <Col key={project.id}>
-            <Card>
-                <Card.Img style={{ objectFit: 'cover', maxHeight: '200px', borderRadius: '0'}} variant="top" src={project.img[0]} alt='sad lego man'/>
-                <Card.Body>
-                    <Card.Title>{project.name}</Card.Title>
-                    <Card.Subtitle>{project.desc}</Card.Subtitle>
-                    <Card.Text>{project.info}</Card.Text>
-                    <Button variant='secondary'>View Project</Button>
-                </Card.Body>
-            </Card>
-        </Col>
+        <>
+            <Col key={project.id}>
+                <Card>
+                    <Card.Img style={{ objectFit: 'cover', maxHeight: '200px', borderRadius: '0' }} variant="top" src={project.img[0]} alt='sad lego man' />
+                    <Card.Body>
+                        <Card.Title>{project.name}</Card.Title>
+                        <Card.Subtitle>{project.desc}</Card.Subtitle>
+                        <Card.Text>{project.info}</Card.Text>
+                        <Button variant='secondary' onClick={handleShow}>View Project</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Modal centered size='lg' show={show} onHide={handleClose} >
+                <Modal.Body>
+                    <Carousel>
+                        {project.img.map((image) => (
+                            <Carousel.Item key={image.id}>
+                                <img src={image} className="d-block h-100 w-100" />
+                                <Carousel.Caption className='photoInfo'>
+                                    <h5>Page name</h5>
+                                    <p>Bottom Text</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </Modal.Body>
+            </Modal>
+        </>
     );
 }
