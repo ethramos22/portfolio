6e409012e981f6d2d3bb03ea931cd55b7a3ce809
ethramos22 <ethramos22@gmail.com>
2025-04-15 11:04:21 -0600

    Spacing changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,11 +12,11 @@ export default function Hero() {
     <div id='hero'>
       <Container className='py-5'>
         <Row>
-          <Col>
+          <Col xs={12} md={6} className='text-center text-md-start'>
             <h1>Ethan Ramos</h1>
             <p>Fullstack Web Developer</p>
           </Col>
-          <Col>
+          <Col xs={12} md={6} className='text-center'>
             <h2 className='plaster-regular brand-big'>ER</h2>
           </Col>
         </Row>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Nav, Navbar, Container } from 'react-bootstrap';
-import { Home } from 'react-feather';
 
 export default function Navigation() {
     return (
